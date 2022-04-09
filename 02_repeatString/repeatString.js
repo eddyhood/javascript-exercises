97fd5997b629e96a704e50a65457edbfa0e736da
Eddy Hood <hood@MacBook-Pro-2.local>
2022-04-09 10:18:47 -0600

    Pass 1 and 2
    Added a fucntion that would take a string and a number, then concat
    that string * number.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let concatString = '';
+    for(i = 0; i < num; i++) {
+        concatString += string;
+    } if(num < 0) {
+        return "ERROR";
+    }return concatString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
