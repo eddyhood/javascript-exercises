8bd5d9339d7171cd72110b30ff8f2ee404c04be0
Eddy Hood <hood@MacBook-Pro-2.local>
2022-04-09 14:54:35 -0600

    Pass removeFromArray test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,15 @@
 const removeFromArray = function(userArray, remove) {
-    const index = userArray.indexOf(remove);
-    if(index > -1) {
-        userArray.splice(index, 1)
-    } return userArray;
-};
+    const argumentLength = arguments.length;
+    for (i = 1; i < argumentLength; i++) {
+        if(userArray.includes(arguments[i])){
+            let index = userArray.indexOf(arguments[i]);
+            if(index > -1) {
+                 userArray.splice(index, 1);
+             } 
+         } 
+         console.log(userArray);
+    }return userArray;
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
