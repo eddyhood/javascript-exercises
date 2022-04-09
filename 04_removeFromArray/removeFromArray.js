c8c0e7f3aee2f9b172bdfa551e946442f49d8ed0
Eddy Hood <hood@MacBook-Pro-2.local>
2022-04-09 11:17:09 -0600

    Pass first test of removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArray, remove) {
+    const index = userArray.indexOf(remove);
+    if(index > -1) {
+        userArray.splice(index, 1)
+    } return userArray;
 };
 
 // Do not edit below this line
