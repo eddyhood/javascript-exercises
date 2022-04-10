032da84192818a5756bca6d6d0c36fabb29d8729
eddyhood <eddy@ignitespot.com>
2022-04-09 22:08:31 -0600

    First attempt to solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    let lowNum = 0;
+    let highNum =0;
+    if(num1 < num2) {
+        lowNum = num1;
+        highNum = num2;
+    } else {
+        lowNum = num2;
+        highNum = num1;
+    }
+    for(i = lowNum; 1 <= highNum; i++){
+        total += i;
+    } return total;
 };
 
 // Do not edit below this line
