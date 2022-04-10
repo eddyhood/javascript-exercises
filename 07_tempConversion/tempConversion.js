c1c9eab92dede03cf2633709ac6648d6fc9a8786
eddyhood <eddy@ignitespot.com>
2022-04-10 13:17:07 -0600

    Solve tempConversion module

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+//[°F] = [°C] × 9⁄5 + 32
+//[°C] = ([°F] − 32) × 5⁄9
 
+const ftoc = function(temp) {
+  let celcius = (temp - 32) * (5/9);
+  return parseFloat(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit = (temp * (9/5)) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
