04a68b2b7890889dacff6972abad8e122f717635
eddyhood <eddy@ignitespot.com>
2022-04-09 22:47:53 -0600

    solved sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,23 @@
 const sumAll = function(num1, num2) {
-    let total = 0;
-    let lowNum = 0;
-    let highNum =0;
-    if(num1 < num2) {
-        lowNum = num1;
-        highNum = num2;
-    } else {
-        lowNum = num2;
-        highNum = num1;
-    }
-    for(i = lowNum; i <= highNum; i++){
-        if(i < 0 || typeof(lowNum) !== 'number' || typeof(highNum) !== 'number'){
-            return 'ERROR';
-        } total += i;
-    } return total;
+    if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
+        return 'ERROR';
+    } else {  
+        let total = 0;
+        let lowNum = 0;
+        let highNum =0;
+        if(num1 < num2) {
+            lowNum = num1;
+            highNum = num2;
+        }else {
+            lowNum = num2;
+            highNum = num1;
+        }
+        for(i = lowNum; i <= highNum; i++){
+            if(i < 0){
+                return 'ERROR';
+            } total += i;
+        } return total;
+    }   
 };
 
 // Do not edit below this line
